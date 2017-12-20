cdbf53971d58ac85d31a285cecd74b87e28f3022
Siegfried <dts7486@163.com>
2017-12-20 16:15:04 +0800

    launch tv and ipfs process on the background

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
   "TVStorageManager/rpc"
   "time"
+  "fmt"
+  "os/exec"
 )
 
 func main() {
 
 	// boot TV
-	
+    tvCmd := exec.Command("Ti_Value.exe", "--daemon", "--data-dir", "C:\\Users\\Siegfried\\AppData\\Roaming\\TVWallet\\chaindata", "--rpcuser", "a", "--rpcpassword", "b", "--rpcendpoint", "127.0.0.1:63695")
+    tvIn, _ := tvCmd.StdinPipe()
+    tvOut, _ := tvCmd.StdoutPipe()
+    tvCmd.Start()
+    fmt.Printf("starting Ti_Value, [PID] %d running...\n", tvCmd.Process.Pid)
+    tvIn.Close()
+    tvOut.Close()
+
 	// boot ipfs
+    ipfsCmd := exec.Command("ipfs.exe", "daemon")
+    ipfsIn, _ := ipfsCmd.StdinPipe()
+    ipfsOut, _ := ipfsCmd.StdoutPipe()
+    ipfsCmd.Start()
+    fmt.Printf("starting ipfs, [PID] %d running...\n", ipfsCmd.Process.Pid)
+    ipfsIn.Close()
+    ipfsOut.Close()
 
 	// boot rpc service
 	go rpc.StartRpcServer()
