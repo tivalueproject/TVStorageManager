337ea57e3ae99d3a4e944184e5bb1839cb797860
DESKTOP-TR9G6RI\Asus <chensiqi_scdy@163.com>
2017-12-21 09:57:43 +0800

    upload file by http post

--- logic/handlefile.go
+++ logic/handlefile.go
@@ -1,12 +1,51 @@
 package logic
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
 )
 
 
-func UploadFile(path string) {
+func UploadFile(filename string, targetUrl string) error {
+	bodyBuf := &bytes.Buffer{}
+    bodyWriter := multipart.NewWriter(bodyBuf)
 
+    fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
+    if err != nil {
+        return err
+    }
 
+    fh, err := os.Open(filename)
+    if err != nil {
+        return err
+    }
+
+    //iocopy
+    _, err = io.Copy(fileWriter, fh)
+    if err != nil {
+        return err
+    }
+
+    contentType := bodyWriter.FormDataContentType()
+    bodyWriter.Close()
+
+    resp, err := http.Post(targetUrl, contentType, bodyBuf)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+    resp_body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return err
+    }
+    fmt.Println(resp.Status)
+    fmt.Println(string(resp_body))
+    return nil
 }
 
 func DownloadFile(hash string) {
