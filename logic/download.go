a58014a2c9baac3cdcffe6c292f2edb3c2d8b5b0
lesterli <lester123@live.cn>
2018-01-02 18:26:30 +0800

    rpc call

--- logic/download.go
+++ logic/download.go
@@ -1,2 +1,27 @@
 package logic
 
+import (
+	"net"
+	TVJson "TVStorageManager/json"	
+)
+
+func ListSaveNodes(params []string, conn net.Conn)(response string) {
+	var strjson string
+	if params != nil {
+		strjson := TVJson.GenerateJsonString("blockchain_get_file_save_node", params)
+	}
+ 	
+	resp := CallRpc(strjson, conn)
+	return resp
+}
+
+func GenerateDownloadValidation(params []string, conn net.Conn)(response string) {
+	var strjson string
+	if params != nil {
+		strjson := TVJson.GenerateJsonString("generate_download_validation", params)
+	}
+ 	
+	resp := CallRpc(strjson, conn)
+	return resp
+}
+
